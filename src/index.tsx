cf499e5219b982b0a0a3a055b7204db3776b5a0b
rado1265 <roberto.diaz.o@outlook.com>
2025-05-01 20:12:07 -0400

    Detecta desktop

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,22 @@ import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home/Home';
 import Condominio from './pages/Condominio/Condominio';
 import HomeDesktop from './pages/HomeDesktop/HomeDesktop';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { isMobile } from 'react-device-detect';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/desktop" element={<HomeDesktop />}></Route>
+      { isMobile ?
       <Route path="/" element={<Home />}></Route>
+      :
+      <Route path="/" element={<HomeDesktop />}></Route>
+      }
       <Route path="/:id/:nombre" element={<Condominio />}></Route>
     </Routes>
     <Analytics/>
