9f3fd44176a3f344c377dc9fe27fc157525014ee
rado1265 <roberto.diaz.o@outlook.com>
2025-05-02 15:27:31 -0400

    New menu

--- src/pages/Condominio/Condominio.tsx
+++ src/pages/Condominio/Condominio.tsx
@@ -7,6 +7,7 @@ import notificacion from './../../components/utils/img/notificacion.png';
 import iconmas from './../../components/utils/img/icon-mas.png';
 import iconmenos from './../../components/utils/img/icon-menos.png';
 import actualizar from './../../components/utils/img/actualizar-flecha.png';
+import menuicon from './../../components/utils/img/menuicon.png';
 import silenciarnotificacion from './../../components/utils/img/silenciar-notificacion.png';
 
 const Condominio = () => {
@@ -37,7 +38,8 @@ const Condominio = () => {
     const [editar, setEditar] = useState(false)
     const [votaciones, setVotaciones] = useState(false)
     const [encuesta, setEncuesta] = useState(false)
-    const [dataVotaciones, setDataVotaciones] = useState([{cabecera: "", opcionesVotacion: []}])
+    const [menuOpciones, setMenuOpciones] = useState(false)
+    const [dataVotaciones, setDataVotaciones] = useState([{ cabecera: "", opcionesVotacion: [] }])
     const [anuncio, setAnuncio] = useState({
         id: 0,
         idCondominio: localStorage.getItem("idCondominio"),
@@ -226,7 +228,6 @@ const Condominio = () => {
         try {
             setLoading(false);
             setDataVotaciones(data);
-            console.log(data);
         } catch (er) {
         }
     }
@@ -258,7 +259,6 @@ const Condominio = () => {
         setCrear(c)
         setEditar(d)
         setEncuesta(e)
-        console.log(a);
         if (a == 5) {
             setLoading(true);
             ObtenerVotacionesLogic(selListadoVotaciones, localStorage.getItem("idCondominio")!.toString(), usuario.id);
@@ -281,7 +281,7 @@ const Condominio = () => {
         const reader = new FileReader();
         reader.onload = (event: any) => {
             const base64 = event.target.result;
-            console.log(base64.replace("data:image/jpeg;base64,", ""))
+            //console.log(base64.replace("data:image/jpeg;base64,", ""))
             if (base64.startsWith('data:image')) {
                 setAnuncio(prev => ({
                     ...prev,
@@ -545,7 +545,11 @@ const Condominio = () => {
 
     const cambiarVoto = (ev: any) => {
         setLoading(true);
-        VotarLogic(selVotar, ev.target.id, usuario.id);
+        if (ev.target.id) {
+            VotarLogic(selVotar, ev.target.id, usuario.id);
+        } else {
+            VotarLogic(selVotar, ev.target.parentNode.id, usuario.id);
+        }
     }
 
     const selVotar = (error: Boolean, err: string, data: any) => {
@@ -556,28 +560,25 @@ const Condominio = () => {
     }
 
     const panelVotaciones = () => {
-        console.log(dataVotaciones);
         return <div style={{ fontFamily: 'Arial, sans-serif' }}>
-            <h2  className="mt-3 mb-4">VOTACIONES ACTIVAS</h2>
+            <h2 className="mt-3 mb-4">VOTACIONES ACTIVAS</h2>
             {dataVotaciones.map((a: any, i: number) => {
                 return (
                     <div className="cardVotacion">
                         <h4 className="text-center">{a.cabecera}</h4>
                         <span className="mb-3 text-center d-block">{a.descripcion}</span>
-                        {a.opcionesVotacion.map((b: any)=>{
-                            console.log(a.total);
-                            console.log(b.votaciones.length);
+                        {a.opcionesVotacion.map((b: any, o: number) => {
                             let percentage = a.total && b.votaciones.length ? (b.votaciones.length / a.total) * 100 : 0;
-                            return (<div key={i} style={{ marginBottom: '10px' }}>
-                            <span>{b.descripcion}</span>
-                            <div id={b.id} style={{ background: '#ddd', height: '25px', width: '100%', borderRadius: '5px', marginTop: '5px' }} onClick={(ev)=> {cambiarVoto(ev);}}>
-                                <div style={{ background: '#4caf50', height: '100%', width: `${percentage}%`, borderRadius: '5px', textAlign: 'center' }}>
-                                    {b.votaciones.find((votacion: any) => votacion.idUsuario == usuario.id) ?
-                                    <span style={{color: 'white', display: 'block', width: '55px', margin: '0 auto'}}>Votado</span>
-                                    :""}
+                            return (<div key={o} style={{ marginBottom: '10px' }}>
+                                <span>{b.descripcion}</span>
+                                <div id={b.id} style={{ background: '#ddd', height: '25px', width: '100%', borderRadius: '5px', marginTop: '5px' }} onClick={(ev) => { cambiarVoto(ev); }}>
+                                    <div style={{ background: '#4caf50', height: '100%', width: `${percentage}%`, borderRadius: '5px', textAlign: 'center' }}>
+                                        {b.votaciones.find((votacion: any) => votacion.idUsuario == usuario.id) ?
+                                            <span style={{ color: 'white', display: 'block', width: '55px', margin: '0 auto' }}>Votado</span>
+                                            : ""}
+                                    </div>
                                 </div>
-                            </div>
-                        </div>);
+                            </div>);
                         })}
                     </div>
                 );
@@ -867,7 +868,7 @@ const Condominio = () => {
                         </div>*/}
                         <div className="w-100 pb-3 mb-3 containerMenu">
                             <div className="containerImgMenu">
-                                {
+                                {/*
                                     usuario.nombre.length > 0 && !usuario.tieneSuscripcion ?
                                         <button className="iconNotificacion" onClick={() => { setLoading(true); SuscribirNotificacionesLogic(selSuscribir, urlPase[3], usuario.id) }}>
                                             <img width={25} src={notificacion} />
@@ -877,12 +878,45 @@ const Condominio = () => {
                                                 <img width={25} src={silenciarnotificacion} />
                                             </button>
                                             : ""
-                                }
+                                */}
+                                <button className="iconNotificacion" onClick={() => { setMenuOpciones(!menuOpciones);}}>
+                                    <img width={25} src={menuicon} />
+                                </button>
                                 <img src={`data:image/jpeg;base64,${dataFull.logo}`} alt="Logo" style={{ width: '65px', margin: '0 auto' }} />
                                 <button className="iconRefresh" onClick={() => { setLoading(true); ObtenerListadoAnuncioLogic(selListadoAnuncios, urlPase[3]); }}>
                                     <img width={25} src={actualizar} />
                                 </button>
                             </div>
+                            { menuOpciones && (
+                            <div className="custom-menu">
+                                <button type="button">
+                                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                                    </svg>
+                                    Profile
+                                </button>
+                                <button type="button">
+                                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m13.5 6H19m-2.25 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 10h11.25m-4.5 6H19M7.75 16a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 16h2.25" />
+                                    </svg>
+                                    Settings
+                                </button>
+                                <button type="button">
+                                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 18" fill="currentColor">
+                                        <path d="M18 4H16V9C16 10.0609 15.5786 11.0783 14.8284 11.8284C14.0783 12.5786 13.0609 13 12 13H9L6.846 14.615C7.17993 14.8628 7.58418 14.9977 8 15H11.667L15.4 17.8C15.5731 17.9298 15.7836 18 16 18C16.2652 18 16.5196 17.8946 16.7071 17.7071C16.8946 17.5196 17 17.2652 17 17V15H18C18.5304 15 19.0391 14.7893 19.4142 14.4142C19.7893 14.0391 20 13.5304 20 13V6C20 5.46957 19.7893 4.96086 19.4142 4.58579C19.0391 4.21071 18.5304 4 18 4Z" fill="currentColor" />
+                                        <path d="M12 0H2C1.46957 0 0.960859 0.210714 0.585786 0.585786C0.210714 0.960859 0 1.46957 0 2V9C0 9.53043 0.210714 10.0391 0.585786 10.4142C0.960859 10.7893 1.46957 11 2 11H3V13C3 13.1857 3.05171 13.3678 3.14935 13.5257C3.24698 13.6837 3.38668 13.8114 3.55279 13.8944C3.71889 13.9775 3.90484 14.0126 4.08981 13.996C4.27477 13.9793 4.45143 13.9114 4.6 13.8L8.333 11H12C12.5304 11 13.0391 10.7893 13.4142 10.4142C13.7893 10.0391 14 9.53043 14 9V2C14 1.46957 13.7893 0.960859 13.4142 0.585786C13.0391 0.210714 12.5304 0 12 0Z" fill="currentColor" />
+                                    </svg>
+                                    Messages
+                                </button>
+                                <button type="button">
+                                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
+                                        <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
+                                    </svg>
+                                    Cerrar Sesión
+                                </button>
+                            </div>
+                            )}
                         </div>
                         <div className="container pb-5 mb-5">
                             <div className="row px-3 px-md-0 justify-content-around">
